ce885d60f21551724895d91042239ec7de1005d0
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-10-30 14:40:15 +0100

    header component done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import "./Header.css"
+import AirbnbLogo from "../assets/airbnb.svg"
 
 
 export default function Header() {
     return (
-        <h1>test</h1>
+        <div className="header">
+            <img src={AirbnbLogo} alt="logo of logo" className="logo" />
+        </div>
     )
 }
