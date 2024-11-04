1cc912ea20066d40627ebf000cab4a55b7586417
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-11-04 16:49:47 +0100

    set upe card array+card file structure

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./Card.css"
+
+export default function Card() {
+    return (
+        <div className="card">
+            
+        </div>
+    )
+}
---
+++ src/components/CardArray.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./CardArray.css"
+import Card from "./Card.jsx"
+
+
+export default function CardArray() {
+    return (
+        <div className="cardArray">
+            <Card />
+        </div>
+    )
+}
