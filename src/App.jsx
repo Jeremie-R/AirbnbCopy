9d28c2cd7245e44cf4ec592f5a45a9bf7759e1dd
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-11-04 16:50:54 +0100

    added card array to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header.jsx"
 import Hero from "./components/Hero.jsx"
+import CardArray from './components/CardArray.jsx'
 
 
 import reactLogo from './assets/react.svg'
@@ -17,6 +18,7 @@ function App() {
     <div>
       <Header />
       <Hero />
+      <CardArray />
     </div>
   )
 }
