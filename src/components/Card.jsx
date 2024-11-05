2c03a054a6aac9e3b57f765e324056f24dbf6708
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-11-05 16:26:26 +0100

    created shell for the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./Card.css"
+import CardDefault from "../assets/CardDefault.png"
+import Star from "../assets/Star.png"
 
 export default function Card() {
     return (
         <div className="card">
-            
+            <div className="chip"><p id="Label">Label</p></div>
+            <img src={CardDefault} alt="card default img" />
+            <div className="cardText">
+                <div className="Row">
+                    <img id="star" src={Star} alt="star" />
+                    <p id="rating">0</p>
+                    <p id="country" className="light">USA</p>
+                </div>
+                <p id="cardTitle">title</p>
+                <div className="Row NoGap">
+                    <p id="price" className="Bold">0</p>
+                    <p> / person</p>
+                </div>
+            </div>
         </div>
     )
 }
