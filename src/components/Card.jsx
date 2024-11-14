132548bc67e08e5bcf71183e87368c50e8037ce4
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-11-14 13:44:20 +0100

    added propst to card element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,19 +5,25 @@ import CardDefault from "../assets/CardDefault.png"
 import Star from "../assets/Star.png"
 
 export default function Card() {
+    const label = "Label";
+    const cardTitle = "Title"
+    const rating = "0";
+    const country = "USA"
+    const price = "12$"
+
     return (
         <div className="card">
-            <div className="chip"><p id="Label">Label</p></div>
+            <div className="chip"><p>{label}</p></div>
             <img src={CardDefault} alt="card default img" />
             <div className="cardText">
                 <div className="Row">
                     <img id="star" src={Star} alt="star" />
-                    <p id="rating">0</p>
-                    <p id="country" className="light">USA</p>
+                    <p>{rating}</p>
+                    <p className="light">{country}</p>
                 </div>
-                <p id="cardTitle">title</p>
+                <p>{cardTitle}</p>
                 <div className="Row NoGap">
-                    <p id="price" className="Bold">0</p>
+                    <p className="Bold">{price}</p>
                     <p> / person</p>
                 </div>
             </div>
